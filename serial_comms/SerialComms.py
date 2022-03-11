cb410e6cab0b06558c06f4c1a35bbf17e813f337
Quinvey Etienne <quinvey@hotmail.com>
2022-03-11 16:09:44 +0100

    that's done
    done like said

--- serial_comms/SerialComms.py
+++ serial_comms/SerialComms.py
@@ -1,21 +1,39 @@
-
+# imports
 import serial
 import time
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.reset_input_buffer()
+    ser = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
+    #ser = serial.Serial('COM6', 9600, timeout=1)
+    #ser.reset_input_buffer()
 
     testOnce = 1
 
     while True:
         if testOnce == 1:
-            ser.write(b"cest")
-            testOnce = 0
+            ser.write(b"\x01")  # move command
+            ser.write(b"\x40")  # 90 degrees angle
+            ser.write(b"\x73")  # 45% speed
+            ser.write(b"\x03")  # 1000cm distance
+            ser.write(b"\xE8")  # 1000cm distance
+            # change test
+            testOnce = 2
 
+        time.sleep(2)
+
+        if testOnce == 2:
+            ser.write(b"\x02")  # rotate command
+            ser.write(b"\x80")  # 180 degrees angle
+            ser.write(b"\x5C")  # 36% speed
+            ser.write(b"\x00")  # unwanted distance
+            ser.write(b"\x39")  # 57cm distance
+            # change test
+            testOnce = 0
 
         line = ser.readline()
         print(line)
+        
+        
 
         time.sleep(1)
     
